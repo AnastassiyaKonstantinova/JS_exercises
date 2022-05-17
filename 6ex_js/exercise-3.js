3a4733e16fcd183d70bc4e68fc255617006cb247
AnastassiyaKonstantinova <anastassiya.zobenko@gmail.com>
2022-05-17 22:07:32 +0200

    feat:more exercises

--- 6ex_js/exercise-3.js
+++ 6ex_js/exercise-3.js
@@ -1,18 +1,30 @@
 function adultFilter(persons) {
-  // ...
+	//for every element of the array we need to select those whose age is egual or above 18 and return them into another array;
+
+	//for every element of the array
+
+	let result = [];
+	for (let i = 0; i < persons.length; i++) {
+		//select those whose age is equal or more than 18
+
+		if (persons[i].age >= 18) {
+			result.push(persons[i]);
+		}
+	}
+	return result;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+	{ name: 'Paul', age: 16 },
+	{ name: 'George', age: 17 },
+	{ name: 'Lucas', age: 21 },
+	{ name: 'Marco', age: 32 },
+	{ name: 'Peter', age: 18 },
+	{ name: 'Carl', age: 13 },
+	{ name: 'Simon', age: 24 },
+	{ name: 'Mark', age: 15 },
+	{ name: 'Sandra', age: 34 },
+	{ name: 'Alice', age: 28 },
 ];
 
 const adults = adultFilter(persons);
