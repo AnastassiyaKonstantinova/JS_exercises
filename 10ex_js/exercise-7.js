75ecbb85d735bff7d68775c7ef6c056debd867c7
AnastassiyaKonstantinova <anastassiya.zobenko@gmail.com>
2022-05-20 10:26:18 +0200

    feat:10ex

--- 10ex_js/exercise-7.js
+++ 10ex_js/exercise-7.js
@@ -1,5 +1,11 @@
 function sumUntil(maxValue) {
-  // ...
+	let resultString = '0';
+	let result = 0;
+	for (let i = 1; i <= maxValue; i++) {
+		result = i + result;
+		resultString = resultString + ' + ' + i;
+	}
+	return resultString + ' = ' + result;
 }
 
 console.log(sumUntil(5));
