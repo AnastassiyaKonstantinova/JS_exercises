3be532c4dab72e6cb62584212c1f401242101235
AnastassiyaKonstantinova <anastassiya.zobenko@gmail.com>
2022-05-20 09:55:12 +0200

    feat:9ex

--- 9ex_js/exercise-6.js
+++ 9ex_js/exercise-6.js
@@ -1,13 +1,18 @@
 function getKeys(obj) {
-  // ...
+	//when iterating though objects, use for...in as a method
+	let result = [];
+	for (let i in obj) {
+		result.push(i);
+	}
+	return result;
 }
 
 const person = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 32,
-  city: 'Rome',
-  job: 'Developer',
+	firstName: 'John',
+	lastName: 'Doe',
+	age: 32,
+	city: 'Rome',
+	job: 'Developer',
 };
 
 const keys = getKeys(person);
